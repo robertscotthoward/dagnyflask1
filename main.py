ea427a0c4056a21f6ba9cc013c7db3d2e3b2ec66
Rob Howard <rob@willo.org>
2022-02-19 19:34:27 -0700

    Absolute Paths

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask import render_template
 
 
-app = Flask('app')
+app = Flask('app',
+            static_folder="/home/rshphx/dagnyflask1/static"
+            template_folder="/home/rshphx/dagnyflask1/templates")
 
 @app.route('/')
 def hello_world():
