715376cbb8febd83ed34a73b83053c1e6f8c0f5c
Rob Howard <rob@willo.org>
2022-02-19 19:47:45 -0700

    Another attempt

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask import render_template
 
 
-app = Flask('app',
-            static_folder="/home/rshphx/dagnyflask1/static",
-            template_folder="/home/rshphx/dagnyflask1/templates")
+app = Flask('app', static_folder="/home/rshphx/dagnyflask1/static", template_folder="/home/rshphx/dagnyflask1/templates")
 
 @app.route('/')
 def hello_world():
